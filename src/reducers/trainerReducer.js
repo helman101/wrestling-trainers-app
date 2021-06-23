d71df2233ce38b2c85400649e874dfa2266fb144
helman101 <helmanandresortegon@gmail.com>
2021-06-23 17:14:11 +0200

    refactor trainer action and reducer

--- src/reducers/trainerReducer.js
+++ src/reducers/trainerReducer.js
@@ -1,6 +1,6 @@
 const SET_TRAINERS = 'SET_TRAINERS';
 
-const trainersReducer = (state = {}, action) => {
+const trainersReducer = (state = [], action) => {
   switch (action.type) {
     case SET_TRAINERS: {
       return action.payload;
