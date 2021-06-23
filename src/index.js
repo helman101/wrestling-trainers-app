98d565e58a412a756f0027fe4226d450dc153c95
helman101 <helmanandresortegon@gmail.com>
2021-06-23 13:29:12 +0200

    update index with reducers and store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import Routes from './routes/Routes';
+import defaultState from './store/store';
+import userReducer from './reducers/userReducer';
+import currentReducer from './reducers/currentReducer';
+import trainersReducer from './reducers/trainerReducer';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  currentTrainer: currentReducer,
+  trainers: trainersReducer,
+});
+
+const store = createStore(rootReducer, defaultState, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
