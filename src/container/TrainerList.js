40a7516e04f41bae2cc602b0da0e10a907db52ab
helman101 <helmanandresortegon@gmail.com>
2021-06-23 17:16:03 +0200

    add useEffect, mapStateToProps and propTypes

--- src/container/TrainerList.js
+++ src/container/TrainerList.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import trainerListRequest from '../API/api';
+import TrainerInfo from '../components/TrainerInfo';
+import changeCurrentTrainer from '../actions/current';
 
 const TrainerList = (props) => {
   const { trainers } = props;
+
+  useEffect(() => {
+    if (trainers.length === 0) {
+      props.dispatch(trainerListRequest);
+    }
+  }, []);
 
   const handleLink = (trainer) => () => {
     props.dispatch(changeCurrentTrainer(trainer));
   };
 
-  if (trainers == null) {
+  if (trainers === []) {
     return (
       <div>
         <h3>Getting Trainers...</h3>
@@ -20,11 +31,20 @@ const TrainerList = (props) => {
         <TrainerInfo
           key={trainer.name}
           trainer={trainer}
-          handleClick={handleLink}
+          handleClick={handleLink(trainer)}
         />
       ))}
     </div>
   );
 };
 
-export default TrainerList;
+TrainerList.propTypes = {
+  trainers: PropTypes.instanceOf(Array).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  trainers: state.trainers,
+});
+
+export default connect(mapStateToProps)(TrainerList);
