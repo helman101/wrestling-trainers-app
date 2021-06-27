891a838a89ec2160c26a8c2344e8f9148341f378
helman101 <helmanandresortegon@gmail.com>
2021-06-27 18:41:18 +0200

    refactor routes and create App container

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import LeftMenu from '../components/LeftMenu';
-import Home from '../components/Home';
-import HowTo from '../components/HowTo';
-import AppointmentList from '../container/AppointmentList';
-import LogIn from '../components/LogIn';
-import SignIn from '../components/SignIn';
-import TrainerList from '../container/TrainerList';
-import TrainerProfile from '../components/TrainerProfile';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../container/App';
 
 const Routes = () => (
   <BrowserRouter>
-    <Navbar />
-    <LeftMenu />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/how_to" component={HowTo} />
-      <Route path="/appointments" component={AppointmentList} />
-      <Route path="/trainerProfile" component={TrainerProfile} />
-      <Route path="/trainers" component={TrainerList} />
-      <Route path="/login" component={LogIn} />
-      <Route path="/signin" component={SignIn} />
-    </Switch>
+    <App />
   </BrowserRouter>
 );
 
