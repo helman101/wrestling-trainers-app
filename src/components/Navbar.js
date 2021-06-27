cb3a905c78ecfbeecb520fa5d4bd4288c9c73a43
helman101 <helmanandresortegon@gmail.com>
2021-06-27 18:44:31 +0200

    improve navbar, now navbar received state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logUserOutAction } from '../actions/user';
+import Logo from '../assets/img/logo.png';
+import styles from '../assets/styles/style.module.css';
 
-const Navbar = () => (
-  <nav>
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2F1000marcas.net%2Fwwe-logo%2F&psig=AOvVaw0HGDo7peC0G6EgMcufigcc&ust=1624460755100000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPjh0YfCq_ECFQAAAAAdAAAAABAD" alt="logo" />
-    <div>
-      <Link to="/login">Log in</Link>
-      <Link to="/signin">Sign Up</Link>
+const Navbar = (props) => {
+  const { user } = props;
+  console.log(user);
+
+  const handleLogOut = () => {
+    props.dispatch(logUserOutAction());
+    props.history.push('/Log_in');
+  };
+
+  const getButtons = () => {
+    if (Object.keys(user).length === 0) {
+      return (
+        <div className={`${styles.mr1}`}>
+          <button className={`${styles.yellowBg} ${styles.navBtn}`} type="button" onClick={() => props.history.push('/Log_in')}>LOG IN</button>
+          <button className={`${styles.yellowBg} ${styles.navBtn} ${styles.ml1} ${styles.mr1}`} type="button" onClick={() => props.history.push('/Sign_up')}>SIGN UP</button>
+        </div>
+      );
+    }
+    return (
+      <div className={`${styles.mr1}`}>
+        <button className={`${styles.yellowBg} ${styles.navBtn}`} type="button" onClick={() => props.history.push('/My_appointments')}>APPOINTMENTS</button>
+        <button className={`${styles.yellowBg} ${styles.navBtn} ${styles.ml1} ${styles.mr1}`} type="button" onClick={handleLogOut}>LOG OUT</button>
+      </div>
+    );
+  };
+
+  return (
+    <div className={`${styles.navbar} ${styles.dFlex} ${styles.alignItemsCenter} ${styles.justifyContentBetween}`}>
+      <div src={Logo} alt="logo" className={`${styles.ml1} ${styles.navLogo}`}>Ohio Valley Wrestling</div>
+      {getButtons()}
     </div>
-  </nav>
-);
+  );
+};
 
-export default Navbar;
+Navbar.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  user: PropTypes.instanceOf(Object).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps)(Navbar);
